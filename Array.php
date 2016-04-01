c9e3b58686c211cbc08a14ec91d3b34198c5bc2e
zhangteng <zhangteng@51shebao.com>
2016-04-01 17:18:16 +0800

    add

--- Array.php
+++ Array.php
@@ -12,6 +12,13 @@ $arr = array(
     'c' => 'happay',
 );
 
-$r_arr = preg_grep("/^o.+$/", $arr);
+$r_arr = preg_grep("/o.*$/", $arr);
+
+//	'a' => string 'hello' (length=5)
+//	'b' => string 'good' (length=4)
+
+$r_arr = preg_grep("/o.+$/", $arr);
+
+//'b' => string 'good' (length=4)
 
 var_dump($r_arr);
